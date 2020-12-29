09c7418df4f748d1e6856e0e7d726709504d6c0c
Ahmed the Green <eng.boshhab@gmail.com>
2020-12-29 23:32:52 +0300

    made a list of movies :)
    movies

---
+++ CW-9-1/CW-9-1/CW_9_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CW_9_1App.swift
+//  CW-9-1
+//
+//  Created by Ahmed Alkhuder on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CW_9_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW-9-1/CW-9-1/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  CW-9-1
+//
+//  Created by Ahmed Alkhuder on 29/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        List
+        {
+            MovieRow(movieName: "Avengers", mainChars: ["captain america","thor", "hulk"])
+            MovieRow(movieName: "Deadpool", mainChars: ["Deadpool himself","wierd people"])
+            MovieRow(movieName: "Terminator", mainChars: ["terminator", "the Conors"])
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct MovieRow: View {
+    var movieName : String
+    var mainChars : [String]
+    
+    var body: some View {
+        HStack(alignment: .top)
+        {
+            Image(movieName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .background(Color.blue)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading)
+            {
+                Text(movieName)
+                    .font(.largeTitle)
+                Text(mainChars.joined(separator: ", "))
+            }
+        }
+    }
+}
